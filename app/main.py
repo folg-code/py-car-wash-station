5b3582696c5f84c238e49af91db3bf16f0bcb506
filip <filip1folga@gmail.com>
2025-05-29 18:36:02 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,47 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(self,
+                 comfort_class: int,
+                 clean_mark: int,
+                 brand: str) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: float,
+                 average_rating: float,
+                 count_of_ratings: float) -> None:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def calculate_washing_price(self, car: Car) -> float:
+        if self.distance_from_city_center != 0:
+            return (car.comfort_class
+                    * (self.clean_power - car.clean_mark)
+                    * (self.average_rating / self.distance_from_city_center))
+
+    def wash_single_car(self, car: Car) -> None:
+        car.clean_mark = self.clean_power
+
+    def serve_cars(self, cars: list) -> float:
+        income = 0
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
+
+        return round(income, 2)
+
+    def rate_service(self, rate: float) -> None:
+        self.count_of_ratings += 1
+        old_rating_component = (self.average_rating
+                                * ((self.count_of_ratings - 1)
+                                   / self.count_of_ratings))
+        new_rating_component = rate * (1 / self.count_of_ratings)
+        self.average_rating = (old_rating_component + new_rating_component)
+        self.average_rating = round(self.average_rating, 1)
